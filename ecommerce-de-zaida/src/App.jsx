e94e7e8cb366e3fa67b0326d5ea4fd6c750e6ef0
Zaida Mora <zaidajmora@gmail.com>
2021-06-24 22:37:18 -0400

    Creacion de navbar con logo y titulo

---
+++ ecommerce-de-zaida/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import { NavbarComponent } from './Components/NavbarComponent';
+
+function App () {
+  return (
+    <div>
+      <NavbarComponent />
+    </div>
+  );
+}
+
+
+export default App;
